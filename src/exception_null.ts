c8373d20981bf2b59297010d98144b9857321978
cobachie <7006340+cobachie@users.noreply.github.com>
2020-07-09 11:34:25 +0900

    Chapter7.4 WIP: Option

--- src/exception_null.ts
+++ src/exception_null.ts
@@ -1,22 +1,22 @@
 function ask() {
-  // return prompt('When is your birthday ?');
-  // return '19780927';
-  return '2100/07/01';
+  let result = prompt('When is your birthday ?');
+  result = '19780927';
+  if (result === null) {
+    return [];
+  }
+  return [result];
 }
 
 /**
  *
  * @param birthday
  */
-function parse(birthday: string): Date | Error {
+function parse(birthday: string): Date[] {
   let date = new Date(birthday);
   if (!isValid(date)) {
-    return new Error('Enter a date in the form YYYY/MM/DD');
+    return [];
   }
-  if (date.getTime() > Date.now()) {
-    return new Error('Are you a timeload?');
-  }
-  return date;
+  return [date];
 }
 
 function isValid(date: Date) {
@@ -26,9 +26,44 @@ function isValid(date: Date) {
   );
 }
 
-let result = parse(ask());
-if (result instanceof Error) {
-  console.error(result.message);
-} else {
-  console.info('Date is', result.toISOString());
+function flatten<T>(array: T[][]): T[] {
+  return Array.prototype.concat.apply([], array);
 }
+
+interface Option<T> {
+  flatMap<U>(f: (value: T) => None): None;
+  flatMap<U>(f: (value: T) => Option<U>): Option<U>;
+  getOrElse(value: T): T;
+}
+
+class Some<T> implements Option<T> {
+  constructor(private value: T) {}
+
+  flatMap<U>(f: (value: T) => None): None;
+  flatMap<U>(f: (value: T) => Some<U>): Some<U>;
+  flatMap<U>(f: (value: T) => Option<U>): Option<U> {
+    return f(this.value);
+  }
+
+  getOrElse(): T {
+    return this.value;
+  }
+}
+
+class None implements Option<never> {
+  flatMap(): None {
+    return this;
+  }
+
+  getOrElse<U>(value: U): U {
+    return value;
+  }
+}
+
+ask()
+  .flatMap(parse)
+  .flatMap((date) => new Some(date.toISOString()))
+  .forEach((date) => new Some('Date is', date))
+  .getOrElse('Error parsing date for some reason.');
+// let date = parse(ask());
+// date.map((_) => _.toISOString()).forEach((_) => console.info('Date is', _));
